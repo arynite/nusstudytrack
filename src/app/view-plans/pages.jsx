2d8c19331a4615713018a1af5a531abbe893b6de
Uwuwenshi <wenshiisgod@gmail.com>
2025-06-17 21:38:20 +0800

    17 June Commit 2

--- src/app/view-plans/pages.jsx
+++ src/app/view-plans/pages.jsx
@@ -1,1 +1,16 @@
-//view plans jsx code
+'use client'
+
+export default function ViewPlans() {
+  return (
+    <div className="view-container">
+      <div className="header">
+        <img src="/nusstlogo.png" alt="Orbital Logo" />
+        <h1>Oh no plans yet, generate table?</h1>
+      </div>
+      
+      <div className="content">
+        <p>This is the view plans page.</p>
+      </div>
+    </div>
+  )
+}
