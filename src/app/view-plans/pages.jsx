850ba61404b3f30b66377de5baef7cc62b643c4f
Uwuwenshi <wenshiisgod@gmail.com>
2025-06-17 21:39:11 +0800

    17 June Commit 3

--- src/app/view-plans/pages.jsx
+++ src/app/view-plans/pages.jsx
@@ -1,5 +1,10 @@
 'use client'
 
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabase } from '@/utils/supabaseClient'
+import './view-plans.css'
+
 export default function ViewPlans() {
   return (
     <div className="view-container">
