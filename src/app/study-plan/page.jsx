90291b83c2057604d18bc117bc6a1d6291d032dc
Uwuwenshi <wenshiisgod@gmail.com>
2025-06-22 14:09:17 +0800

    22 June Commit 9

--- src/app/study-plan/page.jsx
+++ src/app/study-plan/page.jsx
@@ -5,7 +5,7 @@ import { useSearchParams } from 'next/navigation'
 import { useState, useEffect } from 'react'
 import './study-plan.css'
 import { generateTimetable } from '@/utils/generateTimetable'
-import { flattenModules } from 'src/app/utils/flattenModules'
+import { flattenModules } from '@/utils/flattenModules'
 import { eeMajorRequirements } from '@/utils/requirements'
 import { specialisationModules } from '@/utils/requirements'
 
