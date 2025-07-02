fd447635e00e32598fc55728cc4ad49a50e1e1dd
uwuwenshi <wenshiisgod@gmail.com>
2025-07-02 16:41:21 +0800

    2 July Commit 7

--- src/utils/testflattenmods.ts
+++ src/utils/testflattenmods.ts
@@ -2,6 +2,6 @@
 import { eeMajorRequirements, specialisationModules } from './requirements'
 import { flattenModules } from './flattenmodules'
 
-const specialisations = ['robotics', 'iot']
+const specialisations = ['robotics', 'iot'] // mock test inputs for specialisations
 const flattened = flattenModules(eeMajorRequirements, specialisations, specialisationModules)
 console.log(flattened)
