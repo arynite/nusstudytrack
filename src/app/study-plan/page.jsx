2ab74d96d3f28234d2f3f45a11cb8d4a06f19f78
Uwuwenshi <wenshiisgod@gmail.com>
2025-07-06 22:26:23 +0800

    6 July Commit 25

--- src/app/study-plan/page.jsx
+++ src/app/study-plan/page.jsx
@@ -8,6 +8,11 @@ import { useSearchParams } from 'next/navigation'
 import { useState, useEffect } from 'react'
 import './study-plan.css'
 
+import { generateTimetable } from '../../utils/generateTimetable' // change from @
+import { flattenModules } from '../../utils/flattenmodules' // change from @
+import { eeMajorRequirements } from '../../utils/requirements' // change from @
+import { specialisationModules } from '../../utils/requirements' // change from @
+
 export default function StudyPlan() {
   const [mounted, setMounted] = useState(false)
 
