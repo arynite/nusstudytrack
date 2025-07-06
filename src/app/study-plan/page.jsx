e3a728f4e45fbad7a67ab7249f5f14d749266d73
Uwuwenshi <wenshiisgod@gmail.com>
2025-07-06 22:20:36 +0800

    6 July Commit 20

--- src/app/study-plan/page.jsx
+++ src/app/study-plan/page.jsx
@@ -1,7 +1,10 @@
 'use client'
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
+import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import { useState, useEffect } from 'react'
 
 export default function StudyPlan() {
