96d9bc71556e939736fa72b3b886e7f798dd62ef
Uwuwenshi <wenshiisgod@gmail.com>
2025-07-12 21:42:23 +0800

    12 July Commit 8

--- src/utils/generateTimetable.ts
+++ src/utils/generateTimetable.ts
@@ -202,7 +202,11 @@ function parsePrerequisites(prereqTree: PrereqTree): PrereqGroup {
         // Find earliest semester offered and with space
         let placed = false
         for (let sem = 0; sem < semesters; sem++) {
-          if (getYearFromSemester(sem) < info.level) continue; // see which level is the module 
+          //if (getYearFromSemester(sem) < info.level) continue; // see which level is the module 
+          const moduleYear = info.level;
+          const currentYear = getYearFromSemester(sem);
+          if (currentYear + 1 < moduleYear) continue;
+          
           //const offered = info.semesterData.some((s) => s.semester === sem + 1) // check for offered in semester
           if (timetable[sem].length < MAX_MODULES_PER_SEMESTER) {
             timetable[sem].push(mod)
