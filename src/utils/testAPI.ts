70645eb56b95ec68266080a5a4efc77b9a5dbd47
Uwuwenshi <wenshiisgod@gmail.com>
2025-07-21 23:08:24 +0800

    21 July Commit 3

--- src/utils/testAPI.ts
+++ src/utils/testAPI.ts
@@ -2,9 +2,9 @@ import { fetchModuleData } from './nusmodsApi';
 
 async function test() {
   try {
-    const data = await fetchModuleData('PC2020');
+    const data = await fetchModuleData('EE2012');
     console.log(data);
-    console.dir(data.prereqTree, {});
+    console.dir(data.prereqTree, {depth : null});
   } catch (error) {
     console.error('Error:', error);
   }
